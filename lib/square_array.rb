0cba84bfcc92ec6427ecffc1f7a3e489e47c0c98
Miles Higbie <miles.higbie@gmail.com>
2019-11-17 20:27:06 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+    i = 0 
+    array2 = []
+    while i < array.length do 
+       array2.push(array[i] * array[i])
+      i += 1
+    end 
+      return array2
 end
